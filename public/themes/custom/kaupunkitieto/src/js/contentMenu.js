133c5a116dce39141d31c03c5535eae2d8e3ab24
Henri Pulakka <henri.pulakka@wundermanthompson.com>
2021-08-11 16:08:23 +0300

    Accessibility functionalities for content menu.

--- public/themes/custom/kaupunkitieto/src/js/contentMenu.js
+++ public/themes/custom/kaupunkitieto/src/js/contentMenu.js
@@ -3,7 +3,14 @@ document.addEventListener('DOMContentLoaded', function () {
   const navigationLinks = document.getElementsByClassName('content-menu__navigation-link');
   // Bind event for navigation links.
   for (let navigationLink of navigationLinks) {
-    navigationLink.addEventListener('click', openItem);
+    navigationLink.addEventListener('click', () => {
+      openItem(navigationLink);
+    });
+    navigationLink.addEventListener('keydown', (event) => {
+      if (event.key === 'Enter') {
+        openItem(navigationLink);
+      }
+    });
   }
 
 });
@@ -11,15 +18,16 @@ document.addEventListener('DOMContentLoaded', function () {
 /**
  * Active the selected content menu item.
  */
-function openItem() {
+function openItem(link) {
 
   // Already active? Nothing to do here.
-  if (!this.classList.contains('active')) {
-    let id = this.dataset.id;
+  if (!link.classList.contains('active')) {
+    let id = link.dataset.id;
     closeItems(id);
     let currentItem = document.querySelector('.content-menu__item-' + id);
     currentItem.classList.add('active');
-    this.classList.add('active');
+    link.classList.add('active');
+    link.setAttribute('aria-selected', 'true');
   }
 
 }
@@ -38,6 +46,7 @@ function closeItems(currentId) {
     // Remove active class from links.
     for (let activeLink of activeLinks) {
       activeLink.classList.remove('active');
+      activeLink.setAttribute('aria-selected', 'false');
     }
     // Remove active class from items.
     for (let menuItem of menuItems) {
