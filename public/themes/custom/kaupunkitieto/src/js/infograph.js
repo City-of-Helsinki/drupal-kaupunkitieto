01946fd9eadd87613454dd602b4177dea31a8df0
Oskari Kulmala <oskari.kulmala@gmail.com>
2021-12-22 14:40:22 +0200

    KV-30 fix flex layout and add handling for precentages

--- public/themes/custom/kaupunkitieto/src/js/infograph.js
+++ public/themes/custom/kaupunkitieto/src/js/infograph.js
@@ -8,6 +8,10 @@
   Drupal.behaviors.infographLinkBehavior = {
     attach: function (context, settings) {
       $(document).ready(function(){
+        $('.paragraph--type--infograph .infograph__value:contains("%")').html(function(_, html) {
+          return html.split('%').join('<span class="small-char">%</span');
+        });
+
         $('.infograph .infograph__item').on('click', function() {
           location.href = $(this).find('.infograph__value a').attr('href');
         });
