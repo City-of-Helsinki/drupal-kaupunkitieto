dcb98476f49cd7ebf0578fd5529a4976fa5da283
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2024-07-17 13:40:54 +0300

    UHF-9782: Refactored content menu JS and styles.

--- public/themes/custom/hdbt_subtheme/src/js/contentMenu.js
+++ public/themes/custom/hdbt_subtheme/src/js/contentMenu.js
@@ -1,80 +1,70 @@
-(function ($) {
+((Drupal, $) => {
 
-  document.addEventListener('DOMContentLoaded', function () {
+  // Close (all) active content menu items.
+  const closeItems = (currentId) => {
 
-    const navigationLinks = document.getElementsByClassName('content-menu__navigation-link');
-    // Bind event for navigation links.
-    for (let navigationLink of navigationLinks) {
-      navigationLink.addEventListener('click', () => {
-        let id = navigationLink.dataset.id;
-        openItem(id);
+    const activeLinks = document.querySelectorAll('.content-menu__navigation-link.active');
+    const menuItems = document.querySelectorAll('.content-menu__item');
+
+    if (activeLinks) {
+      // Remove active class from links.
+      [...activeLinks].forEach((activeLink) => {
+        activeLink.classList.remove('active');
+        activeLink.setAttribute('aria-selected', 'false');
       });
-      navigationLink.addEventListener('keydown', (event) => {
-        if (event.key === 'Enter') {
-          let id = navigationLink.dataset.id;
-          openItem(id);
+      // Remove active class from items.
+      [...menuItems].forEach((menuItem) => {
+        if (!menuItem.classList.contains(`.content-menu__item-${currentId}`)) {
+          menuItem.classList.remove('active');
         }
       });
     }
+  };
 
-    // Select2 dropdown.
-    $('.content-menu__navigation-select').select2({
-      minimumResultsForSearch: 20,
-    });
-    $('.content-menu__navigation-select').on('select2:select', function () {
-      let id = $('.content-menu__navigation-select option:selected').attr('data-id')
-      openItem(id);
-    });
-
-  });
-
-  /**
-   * Active the selected content menu item.
-   */
-  function openItem(id) {
-    let links = document.querySelectorAll('.content-menu__navigation-link[data-id="' + id + '"]');
+  // Active the selected content menu item.
+  const openItem = (id) => {
+    const links = document.querySelectorAll(`.content-menu__navigation-link[data-id="${id}"]`);
     const navigationSelect = document.querySelector('.content-menu__navigation-select');
 
     // Already active? Nothing to do here.
     if (!links[0].classList.contains('active')) {
       closeItems(id);
-      let currentItem = document.querySelector('.content-menu__item-' + id);
+      const currentItem = document.querySelector(`.content-menu__item-${id}`);
       currentItem.classList.add('active');
 
       navigationSelect.value = id;
-      for (let link of links) {
+
+      [...links].forEach((link) => {
         link.classList.add('active');
         link.setAttribute('aria-selected', 'true');
-      }
+      });
       $('.content-menu__navigation-select').trigger('change');
     }
+  };
 
-  }
+  // Attach table of contents.
+  Drupal.behaviors.contentMenu = {
+    attach() {
+      const navigationLinks = document.getElementsByClassName('content-menu__navigation-link');
 
-  /**
-   * Close (all) active content menu items.
-   *
-   * @param currentId
-   */
-  function closeItems(currentId) {
+      // Bind event for navigation links.
+      [...navigationLinks].forEach((navigationLink) => {
+        navigationLink.addEventListener('click', () => {
+          openItem(navigationLink.dataset.id);
+        });
+        navigationLink.addEventListener('keydown', (event) => {
+          if (event.key === 'Enter') {
+            openItem(navigationLink.dataset.id);
+          }
+        });
+      });
 
-    let activeLinks = document.querySelectorAll('.content-menu__navigation-link.active');
-    const menuItems = document.querySelectorAll('.content-menu__item');
-
-    if (activeLinks) {
-      // Remove active class from links.
-      for (let activeLink of activeLinks) {
-        activeLink.classList.remove('active');
-        activeLink.setAttribute('aria-selected', 'false');
-      }
-      // Remove active class from items.
-      for (let menuItem of menuItems) {
-        if (!menuItem.classList.contains('.content-menu__item-' + currentId)) {
-          menuItem.classList.remove('active');
-        }
-      }
-    }
-
-  }
-
-})(jQuery);
+      // Select2 dropdown.
+      $('.content-menu__navigation-select')
+        .select2({ minimumResultsForSearch: 20 })
+        .on('select2:select', () => {
+          openItem($('.content-menu__navigation-select option:selected').attr('data-id'));
+        });
+    },
+  };
+})(Drupal, jQuery);
