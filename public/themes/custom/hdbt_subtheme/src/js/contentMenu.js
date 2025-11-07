f784c1f20f0562d98244f372c3bc1d3ec03804b9
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2025-11-07 14:37:04 +0200

    UHF-12066: Reformatted JS

--- public/themes/custom/hdbt_subtheme/src/js/contentMenu.js
+++ public/themes/custom/hdbt_subtheme/src/js/contentMenu.js
@@ -1,9 +1,9 @@
 ((Drupal, $) => {
-
   // Close (all) active content menu items.
   const closeItems = (currentId) => {
-
-    const activeLinks = document.querySelectorAll('.content-menu__navigation-link.active');
+    const activeLinks = document.querySelectorAll(
+      '.content-menu__navigation-link.active',
+    );
     const menuItems = document.querySelectorAll('.content-menu__item');
 
     if (activeLinks) {
@@ -23,8 +23,12 @@
 
   // Active the selected content menu item.
   const openItem = (id) => {
-    const links = document.querySelectorAll(`.content-menu__navigation-link[data-id="${id}"]`);
-    const navigationSelect = document.querySelector('.content-menu__navigation-select');
+    const links = document.querySelectorAll(
+      `.content-menu__navigation-link[data-id="${id}"]`,
+    );
+    const navigationSelect = document.querySelector(
+      '.content-menu__navigation-select',
+    );
 
     // Already active? Nothing to do here.
     if (!links[0].classList.contains('active')) {
@@ -45,7 +49,9 @@
   // Attach table of contents.
   Drupal.behaviors.contentMenu = {
     attach() {
-      const navigationLinks = document.getElementsByClassName('content-menu__navigation-link');
+      const navigationLinks = document.getElementsByClassName(
+        'content-menu__navigation-link',
+      );
 
       // Bind event for navigation links.
       [...navigationLinks].forEach((navigationLink) => {
@@ -63,7 +69,11 @@
       $('.content-menu__navigation-select')
         .select2({ minimumResultsForSearch: 20 })
         .on('select2:select', () => {
-          openItem($('.content-menu__navigation-select option:selected').attr('data-id'));
+          openItem(
+            $('.content-menu__navigation-select option:selected').attr(
+              'data-id',
+            ),
+          );
         });
     },
   };
--- public/themes/custom/hdbt_subtheme/src/js/searchToggle.js
+++ public/themes/custom/hdbt_subtheme/src/js/searchToggle.js
@@ -3,8 +3,12 @@
     attach() {
       // Header search form
       // Select the toggle button and the search form
-      const searchFormOpenButton = document.querySelector('.toggle-search-form');
-      const searchFormCloseButton = document.querySelector('.header_search_form__close');
+      const searchFormOpenButton = document.querySelector(
+        '.toggle-search-form',
+      );
+      const searchFormCloseButton = document.querySelector(
+        '.header_search_form__close',
+      );
       const searchForm = document.querySelector('.header-search');
 
       if (searchFormOpenButton) {
@@ -12,7 +16,10 @@
         searchFormOpenButton.addEventListener('click', (e) => {
           e.preventDefault();
           // Toggle the display of the search form
-          if (searchForm.style.display === 'none' || searchForm.style.display === '') {
+          if (
+            searchForm.style.display === 'none' ||
+            searchForm.style.display === ''
+          ) {
             searchForm.style.display = 'block';
           } else {
             searchForm.style.display = 'none';
@@ -32,5 +39,4 @@
       }
     },
   };
-
 })(Drupal);
